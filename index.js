2ccbbcdfe2a1a0aee7ac3e3bd3e1e18bb16bbd25
Brent7788 <brent@LAPTOP-5EH0NGLK>
2021-02-14 19:23:01 +0200

    --

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var FirebaseService_1 = require("./src/services/FirebaseService");
+exports.default = FirebaseService_1.default;
